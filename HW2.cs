f63e8624cc99855b64e9cf23fc0ef807e99322b9
darya <ddmakarova@yandex.ru>
2023-04-14 22:30:57 +0300

    Добавлено решение задачи 2

---
+++ HW2.cs
@@ -0,0 +1,16 @@
+int GetValue(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumRec(int number1, int number2)
+{
+  if (number2 == number1 - 1) return 0;
+  else return number2 + SumRec(number1, number2 - 1);
+}
+
+int m = GetValue("Введите значение M: ");
+int n = GetValue("Введите значение N: ");
+
+Console.WriteLine($"\nСумма натуральных чисел в промежутке от {m} до {n} = {SumRec(m, n)}");
